f6d21ec95016a66cad27bd44f14f4c91731177a4
Max-42 <Max@Oppermann.fun>
2020-11-26 20:32:05 +0100

    +simplification and debuging

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 #!/usr/bin/env python3.9
-#importiert das random package
-import random
-#willkommens Nachicht
-spacer = "#####################################\n"
+
+import random #importiert das random package
+import argparse #importiert das argparse Package um argumente zu verarbeiten
+import logging as log #importiert das logging package als log um die Ausgabe des Scriptes anzupassen.
+from colorama import Fore, Style, init #importiert aus colorama Fore und Style um Farbige ausgabe zu ermöglichen. init um das automatische zurücksetzen zu ermöglichen.
+init(autoreset=True)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--verbose', action='count', default=0)
+args = parser.parse_args()
+
+#Legt die debug Ausgabeatufen fest.
+stufen = [log.WARNING, log.INFO, log.DEBUG]
+stufe = stufen[min(len(stufen)-1,args.verbose)]
+
+log.basicConfig(level=stufe, format= Fore.YELLOW +"%(message)s" + Style.RESET_ALL) #legt das Nachichten format fest
+
+
+#Willkommens Nachicht
+spacer = Fore.BLUE + "#####################################\n"
 print(spacer + "Willkommen bei Schnick Schnack Schnuck\n" + spacer)
 
 liste = ["Stein" , "Papier", "Schere"]
 pc = random.randint(0,2)
 
-print(pc)#Gibt die Auswahl des PC's an. (Zum testen)
+
+log.debug("Auswahl des PC " + str(pc) + " welches " +  str(liste[int(pc)]) + " entspricht.")  #Gibt die Auswahl des PC's an. (Zum testen)
 
 def abfrage():
-    """ Führt die Abfrage des User-Inputs aus. """
+    """ 
+    Führt die Abfrage des User-Inputs aus. 
+    """
     global user
     eingabe = input('Gebe "Schere", "Stein" oder "Papier" ein.\n')
     if eingabe == "":
-
+        print(chr(27) + "[2J")
         print("Du darfst nicht nichts angeben.")
         abfrage()
 
@@ -30,11 +49,11 @@ def abfrage():
             print ("Es ist ein Fehler bei der zuordnug unterlaufen.\nDieser Fehler sollte nie Aufteten da die Eingabe voher geprüft wird.")
     else:
         print("Deine Eingabe ist ungültig\nPrüfe ob du dich vertippt hast.")
-        print(abfrage)
+        log.warning("Falche Eingabe: " + eingabe)
         abfrage()
 
 abfrage()
 
-print(user)
-print("Der User hat " + str(liste[user]) + " gewählt")
-print("Der Pc hat " + str(liste[pc]) + " gewählt")
+log.debug(user)
+log.info("Der User hat " + str(liste[user]) + " gewählt")
+log.info("Der Pc hat " + str(liste[pc]) + " gewählt")
